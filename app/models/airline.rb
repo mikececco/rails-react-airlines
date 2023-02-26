35421a12f7c90edc07581cbe6dc1721be75aa0b8
Mike Cecconello <mikececco2000@gmail.com>
2023-02-26 16:14:56 +0100

    Airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,13 @@
 class Airline < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugify # s called before Base.save on new objects that haven’t been saved yet (no record exists).
+
+  def slugify
+    self.slug = name.parameterize # Replaces special characters in a string so that it may be used as part of a ‘pretty’ URL.
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
 end
