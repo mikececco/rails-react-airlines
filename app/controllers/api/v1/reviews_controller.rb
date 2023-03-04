1a3009bed72f13da7e57dac9832c6e20ca44e293
Mike Cecconello <mikececco2000@gmail.com>
2023-03-04 15:44:01 +0100

    Review controller done

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,29 @@
 module Api
   module V1
     class ReviewsController < ApplicationController
+
+      def create
+        review = Review.new(review)
+
+        if review.save
+          render json: ReviewSerializer.new(review).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+      end
+
+      def desotry
+        review = Review.find(review)
+
+        if review.destroy
+          head :no_content #returns Status Code:200 OK
+        else
+          render json: { error: airline.errors.messages }, status: 422
+      end
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
     end
   end
 end
