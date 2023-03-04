d4f6aaf73881ea136a79eb22c94a2612425a185a
Mike Cecconello <mikececco2000@gmail.com>
2023-03-04 15:29:02 +0100

    routes done

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -48,6 +48,10 @@ module Api
       def airline_params
         params.require(:airline).permit(:name, :image_url)
       end
+
+      def options
+        @options ||= { include: %i[reviews] } # compound document?
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   namespace :api do # API routes in our app, organize controllers
     namespace :v1 do
       resources :airlines, param: :slug
-      resources :reviews, only: [:create, :destroy]
+      resources :reviews, only: %i[create destroy]
     end
   end
 
-  get '*path', to: 'pages#index', via: :all # all not pre-defined path in API will be redirected to index
+  get '*path', to: 'pages#index', via: :all # all not pre-defined path in API will be redirected to index - handle React routes withtout interfering
 end
