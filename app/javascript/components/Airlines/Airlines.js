a9f1d3a644e6f72c5f3f4a552b0aa842db968226
Mike Cecconello <mikececco2000@gmail.com>
2023-03-05 13:18:08 +0100

    Request with axios working

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect} from 'react';
+import axios from "axios";
 
 const Airlines = () => {
-  console.log('Airlines render');
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {//make request and fetch all airlines
+    //Get all airlines from API
+    //update airlines in our state
+    axios.get('/api/v1/airlines.json')
+    .then( resp => console.log(resp) )
+    .catch( resp => console.log(resp) )
+  }, [airlines.length])
 
   return (
     <div>
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Airlines from '../components/Airlines/Airlines';
 import Airline from '../components/Airline/Airline';
 
 const App = () => {
-  const [airlines, setAirlines] = useState([])
-
-  useEffect(() => {//make request and fetch all airlines
-    //Get all airlines from API
-    //update airlines in our state
-  })
-
+  console.log('App render');
   return (
     <Switch>
-      <Route exact path="/" render={() => <Airlines />} />
+      <Route exact path="/" component={Airlines} />
       <Route exact path="/airlines/:slug" component={Airline} />
     </Switch>
   );
