ea4e071bfc236525f344308fc5f84da031fde723
Mike Cecconello <mikececco2000@gmail.com>
2023-03-05 13:08:25 +0100

    use axios to make request to api

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Airlines from '../components/Airlines/Airlines';
 import Airline from '../components/Airline/Airline';
 
 const App = () => {
-  console.log('App render');
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {//make request and fetch all airlines
+    //Get all airlines from API
+    //update airlines in our state
+  })
+
   return (
     <Switch>
       <Route exact path="/" render={() => <Airlines />} />
